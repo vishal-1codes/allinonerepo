a5238ac4d922bddeb55f96360997b6e77006fda1
MacFreaks <MacFreaks@apples-MacBook.local>
2023-03-10 21:47:51 +0530

    add login and register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
